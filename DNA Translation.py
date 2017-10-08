a85783e492ade48fd3daeeb8de30c07818b22128
absanyal <absanyal.ABS@gmail.com>
2017-10-08 05:31:22 +0530

    Beautified

--- DNA Translation.py
+++ DNA Translation.py
@@ -12,23 +12,25 @@ Each codon corresponds to an amino acid, which makes the full protein.
 import random
 import numpy as np
 
-#Checking push
+# Checking push
 
-#Declaration of certain constants
-codon_length = 3   #Length of a codon
-mutation_rate = 0.1   #Rate of mutation out of 1
+# Declaration of certain constants
+codon_length = 3  # Length of a codon
+mutation_rate = 0.1  # Rate of mutation out of 1
 
-#Dictionary of amino-acids
+# Dictionary of amino-acids
 amino_acid = dict([("000", 0),
                    ("001", 1),
-                    ("010", 2),
-                    ("011", 3),
-                    ("100", 4),
-                    ("101", 5),
-                    ("110", 6),
-                    ("111", 7)])
+                   ("010", 2),
+                   ("011", 3),
+                   ("100", 4),
+                   ("101", 5),
+                   ("110", 6),
+                   ("111", 7)])
 
-#Generate a random DNA, which is a sequence of 0's and 1's. The length should be a multiple of 3 in this case (but not necessarily).
+# Generate a random DNA, which is a sequence of 0's and 1's. The length should be a multiple of 3 in this case (but not necessarily).
+
+
 def create_random_dna(length):
     i = 1
     dna = []
@@ -37,7 +39,9 @@ def create_random_dna(length):
         i += 1
     return dna
 
-#Show a formatted sequence where elements are separated by '-'
+# Show a formatted sequence where elements are separated by '-'
+
+
 def show_seq(seq, separator):
     i = 0
     p = ""
@@ -48,7 +52,9 @@ def show_seq(seq, separator):
         i += 1
     return p
 
-#Format DNA into codons
+# Format DNA into codons
+
+
 def show_DNA_seq(seq):
     i = 0
     p = ""
@@ -59,20 +65,22 @@ def show_DNA_seq(seq):
         i += 1
     return p
 
-#Translate each three base block (codon) into an amino acid and form a sequence of amino acids
+# Translate each three base block (codon) into an amino acid and form a sequence of amino acids
+
+
 def translate(dna_original):
     dna = dna_original[:]
-    #Trim the DNA so that excess parts are gone in case the number of bases is not a multiple of codon_length
+    # Trim the DNA so that excess parts are gone in case the number of bases is not a multiple of codon_length
     excess_bases = len(dna) % codon_length
     i = 1
     while (i <= excess_bases):
         dna.pop()
         i += 1
-    #Scan the DNA
+    # Scan the DNA
     i = 0
     protein = []
     while (i < len(dna)):
-        #Scan eaach codon and translate to an amino-acid
+        # Scan eaach codon and translate to an amino-acid
         j = i
         codon = ""
         while (j < i + codon_length):
@@ -82,7 +90,9 @@ def translate(dna_original):
         i += 3
     return protein
 
-#Random rubstitution mutation
+# Random rubstitution mutation
+
+
 def substitute_random(dna_original):
     dna = dna_original[:]
     i = 0
@@ -96,7 +106,9 @@ def substitute_random(dna_original):
         i += 1
     return dna
 
-#Function to test similarity between two DNA strands
+# Function to test similarity between two DNA strands
+
+
 def fitness(s, target):
     i = 0
     k = 0
@@ -105,6 +117,7 @@ def fitness(s, target):
         i += 1
     #k = np.sqrt(k)
     return k
+
 
 def changed_positions(s1, s2):
     positions = []
@@ -122,7 +135,8 @@ def changed_positions(s1, s2):
             i += 1
     return positions
 
-#Run tests
+
+# Run tests
 sample_dna = create_random_dna(1000)
 print("\nThe DNA is:")
 print(show_DNA_seq(sample_dna))
